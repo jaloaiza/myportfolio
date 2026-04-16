14c3c2d46dfdc95155903936c7da14bbde1938a3
Julian Loaiza <ja.loaiza@outlook.com>
2026-04-16 11:57:09 -0700

    Testing Github Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: '#',
-  // base: '/',
+  site: 'https://jaloaiza.github.io',
+  base: '/myportfolio',
   vite: {
     plugins: [tailwindcss()]
   },
