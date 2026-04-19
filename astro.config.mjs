4352e3ac5167e1e3d7c579d2695acfc8a63c25e2
jujuloaiza <loaizajulian@yahoo.com>
2026-04-18 17:04:03 -0700

    Domain Transfer

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jaloaiza.github.io',
-  base: '/myportfolio/',
+  site: 'https://julianloaiza.com',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   },
